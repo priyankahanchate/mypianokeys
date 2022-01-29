7b336c753b7c0bc6c07f1d6652eb0fbff40de109
Priyanka <priyanka.sandeep.navale@gmail.com>
2022-01-30 06:33:10 +1100

    adding file

---
+++ js/index.js
@@ -0,0 +1,106 @@
+const description = document.querySelector('#description');
+const name2 = document.querySelector('#name');
+const assignedPerson = document.querySelector('#assigned-person');
+const dateItself = document.querySelector('#date-input');
+const statusCheck = document.querySelector('#status');
+const submitButton = document.querySelector('#submit-button');
+const goToTasks = document.querySelector('#task-card');
+// const clearButton = document.querySelector('#clear-button')
+// console.log(clearButton)
+const newTask = new TaskManager();
+// newTask.load();
+newTask.render();
+submitButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    // Validate name of person creating new task has more than 5 characters
+    if (name2.value.length > 5) {
+        name2.classList.add('is-valid');
+        name2.classList.remove('is-invalid');
+    } else {
+        name2.classList.add('is-invalid');
+        name2.classList.remove('is-valid');
+        return false;
+    };
+    // Validate description length is more than 5 characters
+    if (description.value.length > 5) {
+        description.classList.add('is-valid');
+        description.classList.remove('is-invalid');
+    } else {
+        description.classList.add('is-invalid');
+        description.classList.remove('is-valid');
+        return false;
+    };
+    // Validate name of person assigned to task has more than 5 characters
+    if (assignedPerson.value.length > 5) {
+        assignedPerson.classList.add('is-valid');
+        assignedPerson.classList.remove
+    } else {
+        assignedPerson.classList.add('is-invalid');
+        assignedPerson.classList.remove('is-valid');
+        return false;
+    };
+    // Validate a correct date is entered
+    const dateSplit = dateItself.value.split(/\D/);
+    console.log(`dateSplit: ${dateSplit}`)
+    const dateValue = new Date(dateSplit[0], --dateSplit[1], ++dateSplit[2]);
+    console.log(`dateValue: ${dateValue}`)
+    // get current date
+    const dateNow = Date.now();
+    console.log(`dateNow: ${dateNow}`)
+    if (dateValue >= dateNow) {
+        dateItself.classList.add('is-valid');
+        dateItself.classList.remove('is-invalid');
+    } else {
+        dateItself.classList.add('is-invalid');
+        dateItself.classList.remove('is-valid');
+        return false;
+    };
+    // Validate a task status has been selected
+    if (statusCheck.value != '') {
+        statusCheck.classList.add('is-valid');
+        statusCheck.classList.remove('is-invalid');
+    } else {
+        statusCheck.classList.add('is-invalid');
+        statusCheck.classList.remove('is-valid');
+        return false;
+    };
+    newTask.addTask(name2.value, description.value, assignedPerson.value, dateItself.value, statusCheck.value);
+    removeFields();
+    newTask.render();
+    // newTask.save()
+})
+function removeFields() {
+    name2.value = '';
+    name2.classList.remove('is-valid');
+    description.value = '';
+    description.classList.remove('is-valid');
+    assignedPerson.value = '';
+    assignedPerson.classList.remove('is-valid');
+    dateItself.value = '';
+    dateItself.classList.remove('is-valid');
+    statusCheck.value = '';
+    statusCheck.classList.remove('is-valid');
+}
+goToTasks.addEventListener('click', (event) => {
+    if (event.target.classList.contains('done-button')) {
+        event.preventDefault();
+        const parentLi = event.target.parentElement.parentElement.parentElement.parentElement;
+        let taskId = Number(parentLi.dataset.taskId);
+        let task = newTask.getTaskById(taskId);
+        let doneButton = document.querySelector('#done-invisible');
+        if (task.status === 'In Progress' || task.status === 'Pending') {
+            task.status = 'Completed';
+            newTask.save();
+            newTask.render();
+        };
+
+    };
+    if (event.target.classList.contains('delete-button')) {
+        event.preventDefault();
+        const parentLi = event.target.parentElement.parentElement.parentElement.parentElement;
+        let taskId = Number(parentLi.dataset.taskId);
+        newTask.delete(taskId);
+        newTask.save();
+        newTask.render();
+    };    
+})
---
+++ js/taskManager.js
@@ -0,0 +1,98 @@
+const createTaskHtml = (id, name, description, assignedTo, dueDate, status) => {
+    const html = `
+    <li id="individual-card" class="card" data-task-id="${id}" style="min-width: 50vw">
+        <div class="card-body">
+            <h5 class="card-title">Name: ${name}</h5>
+            <p class="card-text">
+                Task Description: ${description}
+            </p>
+            <p class="card-text">${assignedTo} is on the job!</p>
+            <p class="card-text">Finish by: ${dueDate}</p>
+            <p class="card-text"><b>Task status: ${status}</b></p>
+            <div class="card-footer row mt-4">
+                <div class="col-6 justify-content-center">
+                    <button id="done-invisible" class="btn btn-outline-success done-button">
+                        Done
+                    </button>
+                </div>
+                <div class="col-6 justify-content-center">
+                    <button class="btn btn-outline-danger delete-button">
+                        Delete
+                    </button>
+                </div>
+            </div>
+        </div>
+    </li>
+    `;
+    return html;
+};
+
+class TaskManager {
+    constructor(currentId = 0) {
+        this.currentId = currentId;
+        this.tasks = [];
+    };
+    addTask(name, description, assignedTo, dueDate, status) {
+        const taskAdd = {
+            id: this.currentId++,
+            name: name,
+            description: description,
+            assignedTo: assignedTo,
+            dueDate: dueDate,
+            status: status,
+        };
+        this.tasks.push(taskAdd);
+    };
+    
+    render() {
+        let tasksHtmlList = [];
+        for (let i = 0; i < this.tasks.length; i++) {
+            const task = this.tasks[i];
+            const date = new Date(task.dueDate);
+            let formattedDate = date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear();
+            let taskHtml = createTaskHtml(task.id, task.name, task.description, task.assignedTo, formattedDate, task.status);
+            tasksHtmlList.push(taskHtml);
+        };
+        // tasksList is tasksHtml in aaron's instructions, task 6, step 2, #4
+        let tasksList = tasksHtmlList.join('\n');
+
+        goToTasks.innerHTML = tasksList;
+    }
+    getTaskById(taskId) {
+        let foundTask;
+        for (let i = 0; i < this.tasks.length; i++) {
+            const task = this.tasks[i];
+            if (task.id === taskId) {
+                foundTask = task;
+            };
+        }
+        return foundTask
+    };
+    save() {
+        let tasksJson = JSON.stringify(this.tasks);
+        localStorage.setItem('tasks', tasksJson);
+        let currentId = JSON.stringify(this.currentId);
+        localStorage.setItem('currentId', currentId);
+    };
+    delete(taskId) {
+        const newTasks = [];
+        for (let i = 0; i < this.tasks.length; i++) {
+            let task = this.tasks[i];
+            if (task.id !== taskId) {
+                newTasks.push(task);
+            };
+        };
+        this.tasks = newTasks;
+    };
+    load() {
+        if (localStorage.getItem('tasks')) {
+            let tasksJson = localStorage.getItem('tasks');
+            this.tasks = JSON.parse(tasksJson);
+        };
+        if (localStorage.getItem('currentId')) {
+            let currentId = localStorage.getItem('currentId');
+            currentId = Number(currentId);
+            this.currentId = JSON.parse(currentId);
+        };
+    };
+}
